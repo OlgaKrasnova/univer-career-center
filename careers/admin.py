d2e8cd806cc68a99e9b0f690dc404943dae8eba9
OlgaKrasnova <krasolga2018@gmail.com>
2021-01-04 23:17:26 +0300

    added database models

--- careers/admin.py
+++ careers/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import DiplomaThesis, Graduate, Resume, Vacancies, Employer, Practice, Events, RequestForPractice, Students, Profession
+
+admin.site.register(DiplomaThesis)
+admin.site.register(Graduate)
+admin.site.register(Resume)
+admin.site.register(Vacancies)
+admin.site.register(Employer)
+admin.site.register(Practice)
+admin.site.register(Events)
+admin.site.register(RequestForPractice)
+admin.site.register(Students)
+admin.site.register(Profession)
