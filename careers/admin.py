89437dccff563a37dcb1194f44472afc04976ad2
OlgaKrasnova <krasolga2018@gmail.com>
2021-01-07 18:38:27 +0300

    adding import and export to vacancies table

--- careers/admin.py
+++ careers/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-
-from .models import DiplomaThesis, Graduate, Resume, Vacancies, Employer, Practice, Events, RequestForPractice, Students, Profession
+from import_export.admin import ImportExportModelAdmin
+from .models import DiplomaThesis, Graduate, Resume, Vacancies, Employer, Practice, Events, RequestForPractice, \
+    Students, Profession
 
 admin.site.register(DiplomaThesis)
 admin.site.register(Graduate)
 admin.site.register(Resume)
-admin.site.register(Vacancies)
+
+
+@admin.register(Vacancies)
+class VacanciesAdmin(ImportExportModelAdmin):
+    list_display = ("image", "title", "description", "id_employer")
+    pass
+
+
 admin.site.register(Employer)
 admin.site.register(Practice)
 admin.site.register(Events)
